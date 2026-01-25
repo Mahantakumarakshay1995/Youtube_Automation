a9f89421c8de8e40b0316df2e4a9c948c45b1e63
Mahantakumarakshay1995 <86707104+Mahantakumarakshay1995@users.noreply.github.com>
2026-01-25 19:12:48 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -16,12 +16,18 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.beans.Transient;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.JavascriptExecutor;
+
 
 public class TestCases extends ExcelDataProvider{ // Lets us read the data
         ChromeDriver driver;
@@ -36,6 +42,143 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
          * Do not change the provided methods unless necessary, they will help in
          * automation and assessment
          */
+        @Test(enabled = true)
+        public void testCase01() throws InterruptedException{
+               System.out.println("Start TC 01"); 
+               driver.get("https://www.youtube.com/");
+                Boolean result=Wrappers.isValidatedUrl(driver);
+                Assert.assertTrue(result);
+
+               WebElement option=driver.findElement(By.xpath("(//yt-icon-button[@id='guide-button'])[1]//button"));
+              Wrappers.scrollAndClick(option,driver);
+               Thread.sleep(2000);    
+
+              WebElement aboutElement=driver.findElement(By.xpath("//div[@id='guide-links-primary']//a[contains(text(),'About')]"));
+              Wrappers.scrollAndClick(aboutElement, driver);
+              Thread.sleep(2000);
+
+              WebElement textElement=driver.findElement(By.xpath("//section[@class='ytabout__content']"));
+              String aboutText=textElement.getText();
+              System.out.println(aboutText);
+              System.out.println("End Of TC 01");
+
+        }
+         @Test(enabled = true)
+        public void testCase02() throws InterruptedException{
+               System.out.println("Start TC 02"); 
+               driver.get("https://www.youtube.com/");
+                Boolean result=Wrappers.isValidatedUrl(driver);
+                Assert.assertTrue(result);
+
+               WebElement option=driver.findElement(By.xpath("(//yt-icon-button[@id='guide-button'])[1]//button"));
+              Wrappers.scrollAndClick(option,driver);
+               Thread.sleep(2000);   
+               
+             WebElement moviesElement= driver.findElement(By.xpath("(//div[@id='items'])[2]//yt-formatted-string[contains(text(),'Movies')]"));
+             Wrappers.scrollAndClick(moviesElement, driver);
+             Thread.sleep(2000);
+
+             WebElement rightarrowElement=driver.findElement(By.xpath("//div[@id='right-arrow']//yt-button-shape"));
+             Wrappers.scrollAndClick(rightarrowElement, driver);
+             Wrappers.Onlyclick(rightarrowElement);
+             Wrappers.Onlyclick(rightarrowElement);
+             Thread.sleep(4000);
+
+             SoftAssert sa=new SoftAssert();
+
+             WebElement dramaElement =driver.findElement(By.xpath("((//ytd-grid-movie-renderer[@class='style-scope yt-horizontal-list-renderer'])[16]//descendant::span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer'])"));
+             Wrappers.waitTillElementClickable(dramaElement, driver);
+             String textActual=dramaElement.getText().replaceAll("[^A-Za-z]", "");//only take word Drama
+             
+             //System.out.println("DramaElement is::"+dramaElement.getText().replaceAll("[^\\d]", ""));//only take number
+             System.out.println("Text Actual is-"+textActual);
+             List<String> textExpected=Arrays.asList("Drama, Action, Romance,Comedy, Actionadventure,Animation");
+                for(String type:textExpected){
+                        if(type.contains(textActual)){
+                                sa.assertTrue(true,"Not Matching with textExpected");
+                        }
+                }
+
+             WebElement AdultElement=driver.findElement(By.xpath("((//ytd-grid-movie-renderer[@class='style-scope yt-horizontal-list-renderer'])[16]//descendant::div[@class='yt-badge-shape__text'])[3]"));
+             String adultTextActual=AdultElement.getText().replaceAll("[^A-Z/]", "");
+             System.out.println(adultTextActual);
+
+             String[] text=adultTextActual.split("/");
+             for(int i=0;i<text.length;i++){
+                if (text[i].equals("A")) {
+                        sa.assertTrue(true, "Contains A");
+                }
+             }
+             if(adultTextActual.contains("A")){
+                System.out.println("Movies is for Adult");
+             }else{
+                System.out.println("Movies is for All child and Adult");
+             }
+             sa.assertAll();
+
+                 System.out.println("End Of TC 02");
+
+        }
+
+         @Test(enabled = true)
+        public void testCase03() throws InterruptedException{
+               System.out.println("Start TC 03"); 
+               driver.get("https://www.youtube.com/");
+                Boolean result=Wrappers.isValidatedUrl(driver);
+                Assert.assertTrue(result);
+
+               WebElement option=driver.findElement(By.xpath("(//yt-icon-button[@id='guide-button'])[1]//button"));
+              Wrappers.scrollAndClick(option,driver);
+               Thread.sleep(3000);   
+               
+             WebElement musicElement= driver.findElement(By.xpath("(//div[@id='items'])[2]//yt-formatted-string[contains(text(),'Music')]"));
+             Wrappers.scrollAndClick(musicElement, driver);
+             Thread.sleep(4000);
+
+             WebElement numberElement =driver.findElement(By.xpath("(//ytd-rich-item-renderer[@class='style-scope ytd-rich-shelf-renderer'])[4]//descendant::div[@class='yt-badge-shape__text']"));
+             Wrappers.scrollOnly(numberElement, driver);
+             System.out.println(numberElement.getText());
+             int numberText=Integer.parseInt(numberElement.getText().replaceAll("[^\\d]",""));
+             
+             SoftAssert sa =new SoftAssert();
+             if (numberText <= 50) {
+                System.out.println("Inside If block");   
+             }
+             sa.assertFalse(numberText <= 50,"Number is equal or below 50");
+             sa.assertAll();
+              System.out.println("End Of TC 03");
+
+        }
+         @Test(enabled = true)
+        public void testCase04() throws InterruptedException{
+               System.out.println("Start TC 04"); 
+               driver.get("https://www.youtube.com/");
+                Boolean result=Wrappers.isValidatedUrl(driver);
+                Assert.assertTrue(result);
+
+               WebElement option=driver.findElement(By.xpath("(//yt-icon-button[@id='guide-button'])[1]//button"));
+              Wrappers.scrollAndClick(option,driver);
+               Thread.sleep(3000);   
+               
+             WebElement showmoreElement= driver.findElement(By.xpath("(//div[@id='items'])[2]//yt-formatted-string[contains(text(),'Show more')]"));
+             Wrappers.scrollAndClick(showmoreElement, driver);
+             Thread.sleep(2000);
+
+             WebElement newsElement= driver.findElement(By.xpath("(//div[@id='items'])[2]//yt-formatted-string[contains(text(),'News')]"));
+             Wrappers.scrollAndClick(newsElement, driver);
+             Thread.sleep(4000);
+
+             int sumText =Wrappers.getSumLikeAndTileText(By.xpath("(//span[@id='title' and contains(text(),'Latest news posts')]//ancestor::div[@id='content'])[2]//child::span[@id='vote-count-middle']"),driver);
+             SoftAssert sa=new SoftAssert();
+             sa.assertTrue(sumText>0,"sum must be greater than 0");
+             sa.assertAll();
+
+              System.out.println("End Of TC 04");
+              
+        }
+        
+
+
         @BeforeTest
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -9,9 +9,86 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Wrappers {
+    ChromeDriver driver;
     /*
      * Write your selenium wrappers here
      */
+    public static Boolean isValidatedUrl(WebDriver driver){
+        Boolean success=null;
+        try {
+            String urlExpected="https://www.youtube.com/";
+           String urlText=driver.getCurrentUrl() ;
+           if(urlText.equals(urlExpected)){
+            success = true;
+           }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Got Exception at TC 01");
+            e.printStackTrace();
+            success = false;
+        }
+        return success;
+    }
+    public static void scrollAndClick(WebElement element, ChromeDriver driver){
+        try{
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+        element.click();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+     public static void scrollOnly(WebElement element, ChromeDriver driver){
+        try{
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+       
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void Onlyclick(WebElement element){
+        element.click();
+    }
+
+    public static void waitTillElementClickable(WebElement element,WebDriver driver){
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static int getSumLikeAndTileText(By locator,WebDriver driver){
+       List<WebElement> numberTextElements=driver.findElements(locator);
+             int sum=0;
+             for(int i=0;i<3;i++){
+               String numberTextString = numberTextElements.get(i).getText().trim().toLowerCase();
+               if(numberTextString.isEmpty()) continue;
+
+              Integer numberTextInt;
+                        if(numberTextString.contains("k")){
+                                //handle 1.2k
+                                numberTextInt=(int)(Double.parseDouble((numberTextString.replace("k", "").replace(",", "")))*1000);
+                        }else{
+                        //handle 999, 1,234
+                        numberTextInt= Integer.parseInt(numberTextString.replace("[^\\d]", ""));
+                        }
+
+               System.out.println(numberTextInt);
+               sum+=numberTextInt;
+               List<WebElement> authorElement=driver.findElements(By.xpath("(//span[@id='title' and contains(text(),'Latest news posts')]//ancestor::div[@id='content'])[2]//child::span[@id='vote-count-middle']//ancestor::div[@class='style-scope ytd-rich-item-renderer']//div[@id='author']"));
+               System.out.println(authorElement.get(i).getText());
+               
+              List<WebElement> textAreaElement=driver.findElements(By.xpath("(//span[@id='title' and contains(text(),'Latest news posts')]//ancestor::div[@id='content'])[2]//child::span[@id='vote-count-middle']//ancestor::div[@class='style-scope ytd-rich-item-renderer']//yt-formatted-string[@id='home-content-text']"));
+               System.out.println(textAreaElement.get(i).getText());
+
+               
+
+             }
+             System.out.println("Sum is :="+sum); 
+             return sum;
+    }
 }
